12ab495acba71cacb6941db1fb84de40ab800eb1
Heorhii Siburov <g.siburov@gmail.com>
2019-09-17 12:49:58 -0700

    app can recieve the token

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,14 +1,56 @@
 import React from 'react';
+import { axiosWithAuth } from '../utilites/axiosWithAuth';
 
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: '',
+      password: ''
+    }
+  };
 
-function Login() {
-  return (
-    <div>
-        <p>
-          Login are here
-        </p>
-    </div>
-  );
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post('/login', this.state.credentials)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem('token', res.data.payload);
+        this.props.history.push('/friends');
+      })
+      .catch(err => console.log('Oh-oh, something wrong', err));
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
